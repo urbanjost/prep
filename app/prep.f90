965a99576577bf7aefb9dd020945354c7f631010
John S. Urban <urbanjost@comcast.net>
2021-12-28 17:39:57 -0500

    minor help text changes

--- app/prep.f90
+++ app/prep.f90
@@ -2013,20 +2013,16 @@ help_text=[ CHARACTER(LEN=128) :: &
 '   The pre-processor prep(1) will interpret lines with "$" (by default) in      ',&
 '   column one, and will output no such lines. Other input is conditionally      ',&
 '   written to the output file based on the directives encountered in the input. ',&
+'                                                                                ',&
 '   It does not support parameterized macros but does support string substitution',&
 '   and the inclusion of free-format text blocks that may be converted to Fortran',&
 '   comments or CHARACTER variable definitions while simultaneously being used   ',&
-'   to generate documentation files. INTEGER or LOGICAL expressions may be used  ',&
-'   to select output lines.                                                      ',&
-'                                                                                ',&
-'   The suggested suffix for Fortran input files is ".ff" for code files unless  ',&
-'   they contain $SYSTEM directives in which case ".FF" is preferred. $INCLUDE   ',&
-'   files should use ".ffinc" and ".FFINC" if they include prep(1) directives.   ',&
-'   This naming convention is not required.                                      ',&
+'   to generate documentation files.                                             ',&
 '                                                                                ',&
 '   An exclamation character on a valid directive begins an in-line comment      ',&
 '   that is terminated by an end-of-line.                                        ',&
 '                                                                                ',&
+'   INTEGER or LOGICAL expressions may be used to select output lines.           ',&
 '   An expression is composed of INTEGER and LOGICAL constants, parameters       ',&
 '   and operators. Operators are                                                 ',&
 '                                                                                ',&
@@ -2038,6 +2034,11 @@ help_text=[ CHARACTER(LEN=128) :: &
 '    C-style:                                                                    ',&
 '     !=     &&     ||                                                           ',&
 !NOT SUPPORTED:   %,  <<,  >>, &,  ~, |
+'                                                                                ',&
+'   The suggested suffix for Fortran input files is ".ff" for code files unless  ',&
+'   they contain $SYSTEM directives in which case ".FF" is preferred. $INCLUDE   ',&
+'   files should use ".ffinc" and ".FFINC" if they include prep(1) directives.   ',&
+'   This naming convention is not required.                                      ',&
 '                                                                                ',&
 '   The syntax for the directive lines is as follows:                            ',&
 '                                                                                ',&
@@ -2086,11 +2087,15 @@ help_text=[ CHARACTER(LEN=128) :: &
 '   define_list, -D define_list  An optional space-delimited list of expressions ',&
 '                                used to define variables before file processing ',&
 '                                commences.                                      ',&
+'                                                                                ',&
 '   -i input_files               The default input file is stdin. Filenames are  ',&
 '                                space-delimited. In a list, @ represents stdin. ',&
+'                                                                                ',&
 '   -o output_file               The default output file is stdout.              ',&
+'                                                                                ',&
 '   -I include_directories       The directories to search for files specified on',&
 '                                $INCLUDE directives.                            ',&
+'                                                                                ',&
 '   --prefix ADE|letter  The default directive prefix character is "$".          ',&
 '                        Alternatives may be specified by providing an           ',&
 '                        ASCII Decimal Equivalent (Common values are 37=%        ',&
@@ -2098,6 +2103,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '                        it is assumed to be a literal character.                ',&
 '                                                                                ',&
 '   --help           Display documentation and exit.                             ',&
+'                                                                                ',&
 '   --verbose        All commands on a $SYSTEM directive are echoed              ',&
 '                    to stderr with a + prefix. Text following the               ',&
 '                    string "@(#)" is printed to stderr similar to               ',&
@@ -2108,18 +2114,22 @@ help_text=[ CHARACTER(LEN=128) :: &
 '                    internal prep(1) variable and then an                       ',&
 '                    environment variable by default. This option                ',&
 '                    turns off testing for environment variables.                ',&
+'                                                                                ',&
 '   --system         Allow system commands on $SYSTEM directives to              ',&
 '                    be executed.                                                ',&
+'                                                                                ',&
 '   --keeptabs       By default tab characters are expanded assuming             ',&
 '                    a stop has been set every eight columns; and                ',&
 '                    trailing carriage-return characters are removed.            ',&
 '                    Use this flag to prevent this processing from               ',&
 '                    occurring.                                                  ',&
+'                                                                                ',&
 '   --comment        try to style comments generated in $BLOCK blocks            ',&
 '                    for other utilities such as doxygen. Default is to          ',&
 '                    prefix lines with ''! ''. Allowed keywords are              ',&
 '                    currently "default", "doxygen","none","ford".               ',&
 '                    THIS IS AN ALPHA FEATURE AND NOT FULLY IMPLEMENTED.         ',&
+'                                                                                ',&
 '   --ident          The output of the $IDENT directive is in the form of a      ',&
 '                    comment by default. If this flag is set the output is       ',&
 '                    of the form described in the $IDENT documentation           ',&
@@ -2127,6 +2137,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '                    for use with the what(1) command. Note this generates an    ',&
 '                    unused variable which some compilers might optimize         ',&
 '                    away depending on what compilation options are used.        ',&
+'                                                                                ',&
 '   -d ignore|remove|blank  Enable special treatment for lines beginning         ',&
 '                           with "d" or "D" The letter will be left as-is        ',&
 '                           (the default); removed; or replaced with a blank     ',&
@@ -2134,7 +2145,9 @@ help_text=[ CHARACTER(LEN=128) :: &
 '                           used to support the optional compilation of          ',&
 '                           "debug" code by many Fortran compilers when          ',&
 '                           compiling fixed-format Fortran source.               ',&
+'                                                                                ',&
 '   --version        Display version and exit                                    ',&
+'                                                                                ',&
 '   --width n        Maximum line length of the output file. The default is 1024.',&
 '                    Typically used to trim fixed-format FORTRAN code that       ',&
 '                    contains comments or "ident" labels past column 72          ',&
@@ -2203,7 +2216,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '   optimization level used when compiling, these strings may or may not         ',&
 '   remain in the object files and executables created.                          ',&
 '                                                                                ',&
-'   Do not use the characters double-quote, greater-than, backslash (">\)        ',&
+'   Do not use the characters double-quote, greater-than, backslash (ie. ">\)    ',&
 '   in the metadata to remain compatible with SCCS metadata syntax.              ',&
 '   Do not use strings starting with " -" either.                                ',&
 '                                                                                ',&
@@ -2215,12 +2228,12 @@ help_text=[ CHARACTER(LEN=128) :: &
 '                                                                                ',&
 '      -append [.true.|.false]                                                   ',&
 '                                                                                ',&
-'   Named files open at the beginning by default. Use the -append switch to      ',&
+'   Files open at the beginning by default. Use the -append switch to            ',&
 '   append to the end of an existing file instead of overwriting it.             ',&
 '                                                                                ',&
 '   $INCLUDE filename                                                            ',&
 '                                                                                ',&
-'   Nested read of specified input file. Fifty (50) nesting levels are allowed.  ',&
+'   Read in specified input file. Fifty (50) nesting levels are allowed.         ',&
 '                                                                                ',&
 '   $PARCEL [name]                                                               ',&
 '                                                                                ',&
@@ -2244,8 +2257,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '                                                                                ',&
 '   IF A $SET DIRECTIVE HAS BEEN DEFINED the "standard" preprocessor values      ',&
 '   ${FILE}, ${LINE}, ${DATE}, and ${TIME} are also available. The time          ',&
-'   data refers to the time of processing, not the current time nor the time     ',&
-'   of compilation or loading.                                                   ',&
+'   refers to the time of processing, not the time of compilation or loading.    ',&
 '                                                                                ',&
 '   $IMPORT names(s)                                                             ',&
 '                                                                                ',&
@@ -2396,7 +2408,9 @@ help_text=[ CHARACTER(LEN=128) :: &
 '                                                                                ',&
 '   $MESSAGE WARNING message                                                     ',&
 '                                                                                ',&
-'   Write message to stderr                                                      ',&
+'   Write message to stderr.                                                     ',&
+'                                                                                ',&
+'   Messages for $MESSAGE do not treat an exclamation as starting a comment      ',&
 '                                                                                ',&
 'LIMITATIONS                                                                     ',&
 '                                                                                ',&
@@ -2406,8 +2420,6 @@ help_text=[ CHARACTER(LEN=128) :: &
 '   $BLOCK is required after a $BLOCK or --file FILENAME is not written.         ',&
 '                                                                                ',&
 '   Nesting of $BLOCK sections not allowed.                                      ',&
-'                                                                                ',&
-'   Messages for $MESSAGE do not treat an exclamation as starting a comment      ',&
 '                                                                                ',&
 '  Input files                                                                   ',&
 '                                                                                ',&
