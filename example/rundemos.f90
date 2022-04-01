2cd8a085c68a797c45cbcb4ffce851aefccfcc8f
John S. Urban <urbanjost@comcast.net>
2022-04-01 15:27:38 -0400

    demos

--- example/rundemos.f90
+++ example/rundemos.f90
@@ -11,12 +11,12 @@ character(len=*),parameter   :: commands(*)=[ character(len=132) :: &
 'prep -i os.ff                      ',&
 'prep -i qsort_mod.ff               ',&
 'prep -i set.ff                     ',&
-'prep -i shell.ff                   ',&
+'prep -i shell.ff --system          ',&
 'prep -i show.ff                    ',&
 'prep -i template.ff                ',&
 'prep -i zen.ff                     ',&
 'prep --help']
-logical                               :: wait
+logical                               :: wait=.false.
 integer                               :: exitstat
 integer                               :: cmdstat
 character(len=256)                    :: cmdmsg
@@ -24,7 +24,8 @@ integer                               :: i
    write(*,g)'ASSUMING prep(1) is in your search path'
    do i=1, size(commands)
       write(*,g)'RUNNING: ',commands(i)
-      call execute_command_line('cd demos;'//commands(i), exitstat=exitstat)
-      print *, i, "Exit status was ", exitstat
+      call execute_command_line('cd demos;'//commands(i), exitstat=exitstat,cmdmsg=cmdmsg,cmdstat=cmdstat,wait=wait)
+      print *, i, "Exit status was ", exitstat,'Command status was ',cmdstat
+      if(exitstat.ne.0)write(*,g)trim(cmdmsg)
    enddo
 end program rundemos
